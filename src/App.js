4a7a07f885d9c640ead9e8e63cb58239103efb9f
ProjectBackupRahul <projectbackuprahul@gmail.com>
2022-12-24 17:32:27 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h2>demo CI CD React app in AWS S3 Bucket </h2> 
+    </div>
+  );
+}
+
+export default App;
